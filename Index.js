9af8d32331b8bf593510eac8cdb7a905dee056f5
TARINIII <115470944+TARINIII@users.noreply.github.com>
2024-06-21 16:52:05 +0530

    Add files via upload

--- Index.js
+++ Index.js
@@ -856,3 +856,518 @@ console.log("**************");
 console.log(typeof((num1*num2).toString()));
 
 // output ok
+
+//Trial Program
+
+var x = [[3,"k","k"],[3,"k","k"]];
+
+var reflist = [0,1,2];
+
+var y = [1,2];
+
+for(i = 0; i < x.length; i++){
+    for(j = 0; j < reflist.length; j++){
+        if(x[i][j] === 3){
+            // pass
+        }
+        else{
+            x[i].splice(j,1,"done");
+            // console.log(x[i][j]);
+        }
+        
+    }
+}
+
+console.log(x);
+// leetcode 46 permutations
+
+/*Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+*/
+
+var nums = [1,2,3];
+
+var targetList = [];
+
+// create a list containing "" the same no of list
+
+var myBlanklist = [];
+
+for(i = 0; i < nums.length; i++){
+    myBlanklist.push("k");
+}
+
+var refList = [];
+
+for(i = 0; i < myBlanklist.length; i++){
+    refList.push(i)
+}
+
+var deleteNo = nums.pop();
+
+
+// console.log(myBlanklist);
+
+// console.log(nums);
+
+// console.log(deleteNo);
+
+
+for(i = 0; i < refList.length; i++){
+    myBlanklist.splice(i,1,deleteNo);
+    for(j = 0; j < nums.length; j++){
+        targetList.push(myBlanklist);
+    }
+    myBlanklist = [];
+    for(k = 0; k < refList.length; k++){
+        myBlanklist.push("k");
+    }    
+}
+
+var cC = 1;
+
+for(i = 0; i < refList.length; i++){
+    if(cC === 0){
+        cC = 1;
+    }
+    else if(cC === 1){
+        cC = 0;
+    } 
+    for(j = 0; j < targetList.length; j++){ 
+        if(targetList[j][i] === deleteNo){
+            // pass
+        }
+        else if(targetList[j][i] === "k"){
+            if(cC === 0){
+                targetList[j].splice(i,1,nums[cC]); 
+            }
+            else if(c === 1){
+                targetList[j].splice(i,1,nums[cC]);
+            }
+            if(cC === 0){
+                cC = 1;
+            }
+            else{
+                cC = 0;
+            }   
+        }
+    }
+}
+
+console.log(targetList);\
+
+// 
+
+var x = [[3,"k","k"],[3,"k","k"],["k",3,"k"],["k",3,"k"],["k","k",3],["k","k",3]];
+
+var refList = [0,1,2];
+
+var nums = [1,2];
+
+var c = 0;
+
+var targetList = [];
+
+function ValueAlter(x){
+    for(i = 0; i < refList.length; i++){
+    if(c === 0){
+        c = 1;
+    }
+    else{
+        c = 0;
+    }
+    for(j = 0; j < x.length; j++){
+        if(x[j][i] === 3){
+            if(c === 0){
+                c = 1;
+            }
+            else{
+                c = 1;
+            }    
+        }
+        else{
+            if(c === 0){
+                x[j].splice(i,1,nums[c]);  
+            }
+            else if(c === 1){
+                x[j].splice(i,1,nums[c]);
+            }
+            if(c === 0){
+                c = 1;
+            }
+            else{
+                c = 0;
+            }      
+        }   
+    }
+    }  
+    for(k = 0; k < x.length; k++){
+        targetList.push(x[k]);
+    }
+    console.log(targetList);
+    console.log("Code working fine");
+}
+ValueAlter([["k","k",3],["k","k",3]]);
+
+//Trial Program
+
+// x = [[3,"k","k"],[3,"k","k"],["k",3,"k"],["k",3,"k"]]
+
+// write a programe to put same index value of 3 to one array
+
+var x = [[3,"k","k"],[3,"k","k"],["k",3,"k"],["k",3,"k"]];
+
+var refList = [0,1,2];
+
+var nums = [1,2];
+
+var c = 0;
+
+var targetList2 = [];
+
+for(i = 0; i < refList.length; i++ ){
+    var targetList1 = [];
+    for(j = 0; j < x.length; j++){
+        if(x[j][i] === 3){
+            targetList1.push(x[j]);
+        }
+    }
+    if(targetList1.length > 0){
+        function valueChange(x){
+            for(l = 0; l < refList.length; l++){
+                for(m = 0; m < x.length; m++){
+                    if(x[m][l] === 3){
+                        // pass
+                    }
+                    else{
+                        if(x[c] !== undefined){
+                            x[m].splice(l,1,nums[c]); 
+                            console.log(c);
+                            c = c + 1;
+                        }
+                        else if(x[c] === undefined){
+                            x[m].splice(l,1,nums[c-1]);
+                            console.log(c);
+                            c = 0;
+                        }
+                    }
+                }
+            }
+            for(n = 0; n < x.length; n++){
+                targetList2.push(x[n]);
+            }
+        }
+        valueChange(targetList1)
+    }
+    else{
+        // pass
+    }
+}
+
+console.log(targetList2);
+
+// wrong output
+
+// Leetcode problem 66
+
+/* You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].*/
+function plusOne(digits){
+
+var to_String = digits.join("");
+
+var to_int = parseInt(to_String)+1;
+
+var to_Array = (to_int.toString()).split("");
+
+console.log(to_Array);
+
+}    
+
+plusOne([9])
+
+// output Ok
+
+// Leetcode no 58 
+
+/* length of the last word
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.*/
+
+function lastWordno(s){
+// var s = "   fly me   to   the moon  ";
+
+var s1 = s.split(" ");
+
+for(i = 0; i < s1.length; i++){
+    if(s1[i].length > 0){
+        var k1 = s1[i].length;
+    }
+}
+
+console.log(k1);
+}
+
+// output ok
+
+// leetcode 217
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+function repeatNo(nums){
+
+for(i in nums){
+    var c = 0;
+    for(j in nums){
+        if(nums[i] === nums[j]){
+            c = c + 1;
+        }
+    }
+    if(c > 1){
+        break;
+    }
+}
+
+if( c > 1){
+    console.log(true);
+}
+else if(c === 1){
+    console.log(false);
+}
+}
+
+// output ok
+
+// leetcode problem 258
+
+/*
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+*/
+
+function addDigit(num) {
+    var arr1 = (num.toString()).split("");
+    c = 0;
+    for(i = 0; i < arr1.length; i++){
+        var i1 = parseInt(arr1[i]);
+        c = c + i1;
+    }
+    return c;
+}
+
+function AddD(num) {
+    var k = addDigit(num);
+    while(((k.toString()).length) > 1){
+        k = addDigit(k);
+    }  
+    console.log(k)
+}
+
+// output ok
+
+
+// leetcode 263 ugly no 
+
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+ 
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+*/
+
+function uglyNo(num) {
+    var i = 2;
+    var c = [];
+    var ref = [2,3,5];
+    var count = 0
+    while(i < (num-1)){
+        if((num) % i === 0){
+            c.push(i);
+        }
+        i = i + 1;
+    }
+    if(c.length > 3){
+        console.log("No");
+    }
+    else if(c.length <= 3 && c.length > 0){
+        for(j in c){
+            if(ref.includes(c[j]) === true){
+                count = count + 1;
+            }
+        }
+        if(count > 0 && count <= 3 && count === c.length){
+            console.log("Yes");
+        } 
+        else{
+            console.log("No")
+        }
+    }
+    else{
+        console.log("No");
+    }
+    console.log(c);
+    console.log(count);
+}
+
+// output ok
+
+
+// leetcode 459 Repeat sub string pattern.
+
+/*
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+*/
+
+var s = "aba";
+
+console.log(s.length);
+
+var k = [];
+
+for(i = 0; i < s.length; i++){
+    if(k.includes(s[i])){
+        break
+    }
+    else{
+        k.push(s[i]); 
+    }
+}
+
+console.log(k);
+
+var c = 0;
+
+for(i = 0; i < s.length; i++){
+    // console.log(s.slice(i,i+3));
+    if(s.slice(i,i+3) === k.join("")){
+        c = c + 1;
+    }
+    else{
+        // pass
+    }    
+}
+
+console.log(c);
+
+if(c * k.length === s.length){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+
+// output ok
+
+
+
+
+
+
